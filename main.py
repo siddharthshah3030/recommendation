d8b91d313f3eb0f51566b262f8424e624e9f4e53
siddharth shah <siddharthshah3030@gmail.com>
2018-12-13 02:45:22 +0530

    completed first phase of item based program results needs to be improved alot

--- main.py
+++ main.py
@@ -28,3 +28,41 @@ ratings['number_of_ratings'].hist(bins=60)
 
 import seaborn as sns
 sns.jointplot(x='rating', y='number_of_ratings', data=ratings)
+
+#visualize
+movie_matrix = data.pivot_table(index='userId', columns='title', values='rating')
+movie_matrix.head()
+
+#sorting ratings
+ratings.sort_values('number_of_ratings', ascending=False).head(10)
+
+AFO_user_rating = movie_matrix['Air Force One (1997)']
+contact_user_rating = movie_matrix['Contact (1997)']
+AFO_user_rating.head()
+contact_user_rating.head()
+similar_to_air_force_one=movie_matrix.corrwith(AFO_user_rating)
+similar_to_air_force_one.head()
+#movies similar to below name
+similar_to_contact = movie_matrix.corrwith(contact_user_rating)
+similar_to_contact.head()
+
+corr_contact = pd.DataFrame(similar_to_contact, columns=['Correlation'])
+corr_contact.dropna(inplace=True)
+corr_contact.head()
+corr_AFO = pd.DataFrame(similar_to_air_force_one, columns=['correlation'])
+corr_AFO.dropna(inplace=True)
+corr_AFO.head()
+
+corr_AFO = corr_AFO.join(ratings['number_of_ratings'])
+corr_contact = corr_contact.join(ratings['number_of_ratings'])
+corr_AFO .head()
+corr_contact.head()
+
+#recommendign top 10 movies 
+corr_AFO[corr_AFO['number_of_ratings'] > 100].sort_values(by='correlation', ascending=False).head(10)
+
+
+corr_contact[corr_contact['number_of_ratings'] > 100].sort_values(by='Correlation', ascending=False).head(10)
+
+
+
