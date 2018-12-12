a49138152e8f740b6b731d019f70d9c946c87ece
siddharth shah <siddharthshah3030@gmail.com>
2018-12-12 14:27:09 +0530

    visualizing and understanding ratings and frequency

--- main.py
+++ main.py
@@ -14,3 +14,17 @@ data = pd.merge(data, movie_titles, on='movieId')
 #getting mean ratin for a movie
 ratings = pd.DataFrame(data.groupby('title')['rating'].mean())
 ratings.head()
+
+# gettign frequency of ratings for a movie
+ratings['number_of_ratings'] = data.groupby('title')['rating'].count()
+ratings.head()
+
+# visualise ratings
+import matplotlib.pyplot as plt
+%matplotlib inline
+ratings['rating'].hist(bins=50)
+
+ratings['number_of_ratings'].hist(bins=60)
+
+import seaborn as sns
+sns.jointplot(x='rating', y='number_of_ratings', data=ratings)
